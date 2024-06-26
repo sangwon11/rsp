4fbdc12191c5f75ecbeeb46edd463d82a1a1761b
sangwon11 <dbstkd0408@gmail.com>
2024-06-26 23:58:33 +0900

    1

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 const Box = (props) => {
-    console.log("props",props)
+    let result;
+    if (
+        props.title === "computer" &&
+        props.result !== "tie" &&
+        props.result !== '' ) {
+            result = props.result === "win" ? "lose" : "win";
+        } else {
+            result = props.result;
+        }
     return (
         <div className='box'>
             <h1>{props.title}</h1>
+            <h2>{props.item && props.item.name}</h2>
             <img 
              className='item-img'
              src={ props.item && props.item.img}/>
-            <h2>{props.result}</h2>
+            <h2>{result}</h2>
         </div>
     );
 }
