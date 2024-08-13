60e22bf6b07d9e5780899ced3e18579533314225
sangwon11 <dbstkd0408@gmail.com>
2024-08-13 16:46:14 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Box from './component/Box';
+
+const choice = {
+  rock: {
+    name: "Rock",
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTozWB4H1jUY8uDzzBlDluQ1oi3qdGsaPtiGg&s'
+  },
+  scissors: {
+    name: "Scissors",
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-S9fVPSeA28SJexVubODPSkYPbaGlHh_Y2g&s'
+  },
+  paper: {
+    name: "Paper",
+    img: 'https://res.cloudinary.com/env-imgs/images/f_auto/shopimages/products/1200/white-card/.jpg'
+  }
+}
 
 function App() {
+
+  const [userSelect, setUserSelect] = useState(null);
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='main'>
+        <Box title='You' item={userSelect} />
+        {/* <Box title='Computer' /> */}
+      </div>
+      <div className='main'>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
     </div>
   );
 }
